5405b69c4ee3ec407a71b28b811b4141e469306c
Josue Morales <molinajosue92@hotmail.com>
2022-04-28 18:48:46 -0400

    Added part of the employee form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,9 @@ import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select'
 
 @NgModule({
   declarations: [
@@ -30,7 +32,9 @@ import { MatSidenavModule } from '@angular/material/sidenav'
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatOptionModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
